9469a8b2120b799256f17646f69d02154fae1086
Charles José Ferrari <svu938@INF.ELTE.HU>
2019-09-25 18:50:51 +0200

    adding class examples

--- 20190918/fibonacci.c
+++ 20190918/fibonacci.c
@@ -14,13 +14,22 @@
 #include <stdio.h>
 int main()
 {
+	
+	//(1+1)=2,(2+1)=3,(3+2)=5
 	int i = 0;
 	int current=1;
 	int previous=0;
 	
+	printf("%i \n", previous);
+	
 	for(i=0; i<20; i++)
 	{
-	    int helper;		
+	    printf("%i \n", current);
+		
+		int helper = current;		
+		current = current + previous;
+		previous = helper;				
+		
 	}
 	
 }
